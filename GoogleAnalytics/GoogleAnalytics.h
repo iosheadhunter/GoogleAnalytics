f6a0e1f9b44c5a50381248c4638b4d881d92cb45
Almaz Ibragimov <almazrafi@gmail.com>
2021-08-15 20:28:08 +0300

    Initial commit

---
+++ GoogleAnalytics/GoogleAnalytics.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoogleAnalytics.
+FOUNDATION_EXPORT double GoogleAnalyticsVersionNumber;
+
+//! Project version string for GoogleAnalytics.
+FOUNDATION_EXPORT const unsigned char GoogleAnalyticsVersionString[];
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIEcommerceFields.h>
+#import <GoogleAnalytics/GAIEcommerceProduct.h>
+#import <GoogleAnalytics/GAIEcommerceProductAction.h>
+#import <GoogleAnalytics/GAIEcommercePromotion.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAILogger.h>
+#import <GoogleAnalytics/GAITrackedViewController.h>
+#import <GoogleAnalytics/GAITracker.h>
---
+++ GoogleAnalytics/GoogleAnalytics.m
@@ -0,0 +1,4 @@
+#import "GAI.h"
+
+@interface GAI (Dummy) @end
+@implementation GAI (Dummy) @end
